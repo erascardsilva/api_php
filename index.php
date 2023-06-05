a886cadbcf23f41b980a1da1a20e95f1d3e1c10c
Erasmo Cardoso <erasmo@vmi-informatica.com.br>
2023-06-05 10:56:38 -0300

    modified:   index.php

--- index.php
+++ index.php
@@ -8,20 +8,12 @@
             display: flex;
             justify-content: center;
             align-items: center;
-<<<<<<< HEAD
-            height: 80vh;
-=======
             height: 90vh;
->>>>>>> refs/remotes/origin/master
             font-family: Arial, sans-serif;
         }
 
         form {
             width: 300px;
-<<<<<<< HEAD
-                       
-=======
->>>>>>> refs/remotes/origin/master
         }
 
         .resultado {
@@ -49,19 +41,18 @@
 
 <body>
     <div>
-<<<<<<< HEAD
         <h1>
             <center>Busca por nome IBGE</center>
         </h1>
         <h3> Retorna quantidade de registro por ano </h3>
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <label for="nome">Nome : </label>
-=======
+
        <h1> <center>Busca por nome IBGE</center></h1>
        <h3> Retorna quantidade de registro por ano </h3> 
         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <label for="nome">Nome:</label>
->>>>>>> refs/remotes/origin/master
+
             <input type="text" name="nome" id="nome" required>
             <br><br>
             <input type="submit" value="Enviar">
@@ -90,19 +81,12 @@
 
                     if ($dados !== null && is_array($dados)) {
                         echo '<div class="resultado">';
-<<<<<<< HEAD
                         echo '<h4>' . "Listando de 10 em 10 anos  : " . '</h4>';
                         
                         foreach ($dados as $item) {
                             echo '<div class="resultado-item">';
                             echo '<br>';
                             echo 'Nome   ' . $item['nome'] . '  :  ' . '<br>';
-=======
-                        foreach ($dados as $item) {
-                            echo '<div class="resultado-item">';
-                            echo '<br>';
-                            echo $item['nome'] . '  : ' . '<br>';
->>>>>>> refs/remotes/origin/master
 
                             if (isset($item['res'])) {
                                 foreach ($item['res'] as $res) {
@@ -122,11 +106,7 @@
                     echo 'Erro ao obter os dados da API.';
                 }
             } else {
-<<<<<<< HEAD
-                echo "O campo nome é obrigatório....";
-=======
-                echo "O campo nome é obrigatório...";
->>>>>>> refs/remotes/origin/master
+                echo "O campo nome é obrigatório..";
             }
         }
         ?>
